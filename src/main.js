a2ae9f63a60ec8d1fedbd06e477f30477aa7cf4e
Zakaraie El Mejdki <elmejdki.zakariae@gmail.com>
2020-10-28 06:29:39 +0100

    Get user City Image

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const apiKeys = {
   ipGeolocation: '7202d97c1c3e489392a751327f3ccb8f',
   openWeather: '6d45528c2a76ce912279f18cb573306c',
+  unsplash: 'PteMhxfvY7LXCzelxMqDvwwojo-b0Yyib05cYQ48v4I',
 };
 
 async function getUserCity() {
@@ -32,12 +33,27 @@ async function getTodayWeather(city) {
   return weatherData;
 }
 
+async function getCityImage(city) {
+  const response = await fetch(
+    `https://api.unsplash.com/photos/random?count=1&query=${city}&client_id=${apiKeys.unsplash}`,
+  );
+
+  if (response.status === 404) {
+    throw new Error('city not found');
+  }
+
+  const [{ urls: { regular } }] = await response.json();
+
+  return regular;
+}
+
 async function run() {
   try {
     const userCity = await getUserCity();
     const weatherData = await getTodayWeather(userCity);
+    const cityImage = await getCityImage(userCity);
 
-    console.log(weatherData);
+    console.log(weatherData, cityImage);
   } catch (err) {
     console.log(err.message);
   }
