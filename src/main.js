408d7899c7b27a828609ecc03818fea34b655b47
Zakaraie El Mejdki <elmejdki.zakariae@gmail.com>
2020-10-28 06:28:08 +0100

    Get Today weather details

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const apiKeys = {
   ipGeolocation: '7202d97c1c3e489392a751327f3ccb8f',
+  openWeather: '6d45528c2a76ce912279f18cb573306c',
 };
 
 async function getUserCity() {
@@ -8,11 +9,35 @@ async function getUserCity() {
   return city;
 }
 
+function getUnit() {
+  const unit = localStorage.getItem('unit');
+  if (unit && unit === 'F') {
+    return 'F';
+  }
+
+  localStorage.setItem('unit', 'C');
+  return 'C';
+}
+
+async function getTodayWeather(city) {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&${getUnit() === 'C' ? 'units=metric' : ''}&appid=${apiKeys.openWeather}`,
+  );
+
+  if (response.status === 404) {
+    throw new Error('City not found');
+  }
+
+  const weatherData = await response.json();
+  return weatherData;
+}
+
 async function run() {
   try {
     const userCity = await getUserCity();
+    const weatherData = await getTodayWeather(userCity);
 
-    console.log(userCity);
+    console.log(weatherData);
   } catch (err) {
     console.log(err.message);
   }
